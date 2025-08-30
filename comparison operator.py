9e7967ece6bbed686574f58fd1c663d7ad53bae5
Mayank Patel <161114314+Mayankpatel-10@users.noreply.github.com>
2025-08-30 12:06:26 +0530

    Add files via upload

---
+++ comparison operator.py
@@ -0,0 +1,22 @@
+a = 10
+b = 20
+
+print("a =", a, " b =", b)
+
+# Equal to
+print("a == b:", a == b)
+
+# Not equal to
+print("a != b:", a != b)
+
+# Greater than
+print("a > b:", a > b)
+
+# Less than
+print("a < b:", a < b)
+
+# Greater than or equal to
+print("a >= b:", a >= b)
+
+# Less than or equal to
+print("a <= b:", a <= b)
